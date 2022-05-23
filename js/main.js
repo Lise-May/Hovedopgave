003f2c507104afe918ea90b1e60f9cc69a749090
Lise-May <lisemaybyfuglien@gmail.com>
2022-05-23 02:53:07 +0200

    rettelser
    Uploadet billeder, fik mest solgte produkter til at virke samt små rettelser

--- js/main.js
+++ js/main.js
@@ -20,11 +20,11 @@ function top_sold_product_btn() {
     var x = document.getElementById("indor_product");
     var y = document.getElementById("outdor_product");
     if (x.style.display === "none") {
-      x.style.display = "block";
+      x.style.display = "flex";
       y.style.display = "none";
     } else {
       x.style.display = "none";
-      y.style.display = "block";
+      y.style.display = "flex";
     }
 }
 
